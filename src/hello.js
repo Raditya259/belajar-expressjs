5a20f0b38dad4b84ad48eb7ae63a5fa8f808a1e5
Raditya Lungguk Satya Putra <radityalungguk@gmail.com>
2023-11-28 17:24:50 +0700

    -

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/Next', (req, res) => {
+    res.send("Hello Next");
+});
+
 app.listen(3000, () => {
     console.log("Server is running on Port 3000");
 }); 
